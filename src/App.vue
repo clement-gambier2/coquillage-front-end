204edbfad8ffaee0cf6722f04cdb9b979cea3e4e
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-22 22:27:12 +0200

    print raw data

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,33 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>
-            Clément
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            MEVN Full Stack App
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item-group
-          color="primary">
-          <v-list-item
-            v-for="(item, i) in items" :key="i" :to="item.link" link>
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-
-    
-    </v-navigation-drawer>
-
-
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <router-view>
-
-      </router-view>
-    </v-main>
-  </v-app>
+  <main>
+    <h3>Coquillage</h3>
+    <p>Just print the data from the API</p>
+    <div>
+      <h4>Users</h4>
+      <li v-for="user in users" :key="user._id">{{user.name}}</li>
+    </div>
+    <div>
+      <h4>Available</h4>
+      <li v-for="available in availables" :key="available._id">{{available.day.slice(8,10)}} / {{available.day.slice(5,7)}}</li>
+    </div>
+  </main>
+  
 </template>
 
 <script>
+import API from './api';
   export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-        { title: 'Home', icon: 'mdi-home', link:"/" },
-        { title: 'Add Post', icon: 'mdi-note-plus', link:"/add-post" },
-        { title: 'About', icon: 'mdi-help-box', link:"/" },
-        
-      ],
-    }),
-    
-  }
+    name: 'App',
+    data(){
+      return{
+        users: [],
+        availables: []
+      };
+    },
+    async created(){
+      this.users = await API.getAllUser();
+      this.availables = await API.getAllAvailable();
+    },
+  };
+  
 </script>
