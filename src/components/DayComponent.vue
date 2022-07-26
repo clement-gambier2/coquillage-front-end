d3460b609c4e86dd95225fb4e9b2e37e239f0a61
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-26 13:01:10 +0200

    delete

--- src/components/DayComponent.vue
+++ src/components/DayComponent.vue
@@ -4,6 +4,7 @@
         
         <v-card>
             <v-card-title>{{available.day.slice(8,10)}} / {{available.day.slice(5,7)}}</v-card-title>
+            <v-btn @click="deleteAvailable(available.id)">Delete</v-btn>
         </v-card>
         
         
@@ -19,6 +20,12 @@
     import API from '../api';
     export default{
         props: ['available'],
+        methods:{
+            async deleteAvailable(id){
+                const response = await API.deleteAvailable(id);
+                this.$router.push({name: 'home', params:{message: response.message}});
+            }
+        }
         
         
     }
--- src/views/MyAvailabilitiesView.vue
+++ src/views/MyAvailabilitiesView.vue
@@ -3,7 +3,8 @@
         <!-- If we use the global variable when there will be the account system. -->
         <!-- <p>{{this.$name}}</p> -->
         <v-container>
-            <p>Mes disponibilités</p>    
+            <h3>{{name.name}}</h3>
+            <h4>Mes disponibilités</h4>    
             <DayComponent v-for="available in availables" :key="available._id" :available="available" />
             
         </v-container>
@@ -14,8 +15,6 @@
             <h4>Ajouter une disponibilité</h4>
 
             <v-form ref="form" @submit.prevent="submitForm" class="pa-5" enctype="multipart/form-data">
-            
-            <!-- <v-date-picker color="green lighten-1" v-model="available.day"></v-date-picker> -->
             <input type="date" v-model="available.day" />
             <v-btn type="submit" class="mt-3" color="primary">Ajouter une disponibilité</v-btn>
            </v-form>
@@ -40,6 +39,7 @@
         data() {
             return {
                 availables: [],
+                name:'',
                 available: {
                     day: '',
                     peopleId: ''
@@ -49,16 +49,15 @@
         async created() {
             this.user = await this.$route.params.user
             this.availables = await API.getAvailableByPeopleId(this.user);
+            this.name = await API.getUserByID(this.user);
             
         },
         methods:{
             async submitForm(){                
                 this.available.day = this.available.day + 'T00:00:00.000Z';
                 this.available.peopleId = await this.$route.params.user;
-                    
                 const response = await API.addAvailable(this.available);
                 this.$router.push({name: 'home', params:{message: response.message}});
-                
             }
         },
         components: { DayComponent }
