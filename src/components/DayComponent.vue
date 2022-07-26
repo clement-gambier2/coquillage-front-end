9c5fbbf918a5bb5691e15587470115fac928abbb
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-26 11:50:11 +0200

    new components

---
+++ src/components/DayComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <v-container>
+        
+        <v-card>
+            <v-card-title>{{available.day.slice(8,10)}} / {{available.day.slice(5,7)}}</v-card-title>
+        </v-card>
+        
+        
+    </v-container>
+    
+
+</template>
+
+
+
+
+<script>
+    import API from '../api';
+    export default{
+        props: ['available'],
+        
+        
+    }
+    
+
+</script>
---
+++ src/views/NamesView.vue
@@ -0,0 +1,26 @@
+<template>
+     
+     <div>
+        <h4>Users</h4>
+        <v-btn color="red" v-for="user in users" :key="user._id"  :to="{name:'availabilities', params: {user: user.id }}">{{user.name}}</v-btn> 
+    </div>
+
+</template>
+
+
+<script>
+    import API from '../api';
+    export default {
+        name: "NamesView",
+        data() {
+            return {
+                users: [],
+            };
+        },
+        async created() {
+            this.users = await API.getAllUser();
+            
+        },
+        
+    };
+</script>
