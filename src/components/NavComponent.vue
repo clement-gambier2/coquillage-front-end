128f5c3dc1ba3d289493f5754b9dcf4b00d87b4f
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-27 23:39:45 +0200

    Create NavComponent.vue

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        
+      <img src="../assets/seashell.png" alt="">
+    </header>
+</template>
+
+
+
+<style>
+  header{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0px 20px;
+    
+    
+  }
+  img{
+    width: 15%;
+    height: auto;
+  }
+</style>
