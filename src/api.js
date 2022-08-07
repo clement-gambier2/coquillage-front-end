bba86b24a5a5090535fdacd4c119cd8fb610f9b8
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-08-07 20:43:56 +0200

    API URL

--- src/api.js
+++ src/api.js
@@ -1,36 +1,37 @@
 import axios from 'axios';
 const url_user = "/people";
 const url_available = "/available";
+const url_api = 'https://coquillage-api.herokuapp.com';
 
 export default class API{
     //Users part
     // to get all the posts from the server
     static async getAllUser(){
-        const res = await axios.get(url_user);
+        const res = await axios.get(url_api + url_user);
         return res.data;
     }
     
     // to get single post by id 
     static async getUserByID(id){
-        const res = await axios.get(`${url_user}/${id}`);
+        const res = await axios.get(`${url_api +url_user}/${id}`);
         return res.data;
     }
     // to insert post into database
     static async addUser(user){
-        const res = await axios.post(url_user, user);
+        const res = await axios.post(url_api +url_user, user);
         return res.data;
     }
 
     // to update post by id
     static async updateUser(id, user){
-        const res = await axios.patch(`${url_user}/${id}`, user);
+        const res = await axios.patch(`${url_api +url_user}/${id}`, user);
         return res.data;
     }
 
 
     // to get single post by id 
     static async deleteUser(id){
-        const res = await axios.delete(`${url_user}/${id}`);
+        const res = await axios.delete(`${url_api +url_user}/${id}`);
         return res.data;
     }
 
@@ -39,31 +40,31 @@ export default class API{
 
     // to get all the posts from the server
     static async getAllAvailable(){
-        const res = await axios.get(url_available);
+        const res = await axios.get(url_api +url_available);
         return res.data;
     }
     
     // to get single post by id 
     static async getAvailableByID(id){
-        const res = await axios.get(`${url_available}/${id}`);
+        const res = await axios.get(`${url_api +url_available}/${id}`);
         return res.data;
     }
     //to get the availibillity of a user using his people id
     static async getAvailableByPeopleId(id){
-        const res = await axios.get(`${url_available}/bypeople/${id}`);
+        const res = await axios.get(`${url_api +url_available}/bypeople/${id}`);
         return res.data;
     }
 
     //to get the people using his date
     static async getPeopleByDay(date){
-        const res = await axios.get(`${url_available}/byday/${date}`);
+        const res = await axios.get(`${url_api +url_available}/byday/${date}`);
         return res.data;
     }
 
     // to insert post into database
     static async addAvailable(available){
         console.log(available);
-        const res = await axios.post(url_available,{
+        const res = await axios.post(url_api +url_available,{
             peopleId: available.peopleId,
             day: available.day,
         });
@@ -72,14 +73,14 @@ export default class API{
 
     // to update post by id
     static async updateAvailable(id, available){
-        const res = await axios.patch(`${url_available}/${id}`, available);
+        const res = await axios.patch(`${url_api +url_available}/${id}`, available);
         return res.data;
     }
 
 
     // to get single post by id 
     static async deleteAvailable(id){
-        const res = await axios.delete(`${url_available}/${id}`);
+        const res = await axios.delete(`${url_api +url_available}/${id}`);
         return res.data;
     }
 }
